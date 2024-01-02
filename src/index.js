b56c6e6999960d7bfee406d80f9d595919fce7b3
MarlyssonProg <designprogthe@gmail.com>
2024-01-01 23:35:42 -0300

    Reformulando a sintaxe do aninhamento dos IFS

--- src/index.js
+++ src/index.js
@@ -6,31 +6,31 @@ if (quantExperiencia <= 1000) {
     classificacao = 'Ferro'
     //console.log('Nível ' + classificacao)
 }
-if (quantExperiencia > 1000 && quantExperiencia <= 2000) {
+else if (quantExperiencia > 1000 && quantExperiencia <= 2000) {
     classificacao = 'Bronze'
     //console.log('Nível ' + classificacao)
 }
-if (quantExperiencia > 2000 && quantExperiencia <= 5000) {
+else if (quantExperiencia > 2000 && quantExperiencia <= 5000) {
     classificacao = 'Prata'
     //console.log('Nível ' + classificacao)
 }
-if (quantExperiencia > 5000 && quantExperiencia <= 7000) {
+else if (quantExperiencia > 5000 && quantExperiencia <= 7000) {
     classificacao = 'Ouro'
     //console.log('Nível ' + classificacao)
 }
-if (quantExperiencia > 7000 && quantExperiencia <= 8000) {
+else if (quantExperiencia > 7000 && quantExperiencia <= 8000) {
     classificacao = 'Platina'
     //console.log('Nível ' + classificacao)
 }
-if (quantExperiencia > 8000 && quantExperiencia <= 9000) {
+else if (quantExperiencia > 8000 && quantExperiencia <= 9000) {
     classificacao = 'Ascendente'
     //console.log('Nível ' + classificacao)
 }
-if (quantExperiencia > 9000 && quantExperiencia <= 10000) {
+else if (quantExperiencia > 9000 && quantExperiencia <= 10000) {
     classificacao = 'Imortal'
     //console.log('Nível ' + classificacao)
 }
-if (quantExperiencia > 10000) {
+else (quantExperiencia > 10000) {
     classificacao = 'Radiante'
     //console.log('Nível ' + classificacao)
 }
